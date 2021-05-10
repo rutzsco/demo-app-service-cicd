d8cee85e8a72f68ba4f49bced03e9f36c785d939
Scott Rutz <scrutz@microsoft.com>
2021-05-10 07:56:42 -0500

    cp

---
+++ Demo.API/Controllers/StatusController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, string>> Get()
+        {
+            var variables = new List<KeyValuePair<string,string>>();
+            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+                variables.Add(new KeyValuePair<string, string>(de.Key.ToString(), de.Value.ToString()));
+            return variables;
+        }
+    }
+}
