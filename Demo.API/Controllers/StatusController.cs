4099a3e00a395054273a82000227878dfa145201
Scott Rutz <scrutz@microsoft.com>
2021-05-11 09:30:19 -0500

    Update StatusController.cs

--- Demo.API/Controllers/StatusController.cs
+++ Demo.API/Controllers/StatusController.cs
@@ -9,13 +9,24 @@ using System.Threading.Tasks;
 namespace DemoAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
     public class StatusController : Controller
     {
-        [HttpGet]
-        public IEnumerable<KeyValuePair<string, string>> Get()
+        [HttpGet("status")]
+        public IActionResult Get()
         {
-            var variables = new List<KeyValuePair<string,string>>();
+            var version = "Unknown";
+            var variables = Environment.GetEnvironmentVariables();
+            if (variables.Contains("APPLICATION_VERSION"))
+                version = variables["APPLICATION_VERSION"].ToString();
+
+            var vm = new { Version = version };
+            return new OkObjectResult(vm);
+        }
+
+        [HttpGet("status/full")]
+        public IEnumerable<KeyValuePair<string, string>> GetFull()
+        {
+            var variables = new List<KeyValuePair<string, string>>();
             foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
                 variables.Add(new KeyValuePair<string, string>(de.Key.ToString(), de.Value.ToString()));
             return variables;
